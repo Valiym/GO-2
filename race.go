8555765c8e02b22d302208aac3458b1d5ef1d335
ildar valeev <94433322+Valiym@users.noreply.github.com>
2022-04-13 00:31:53 +0300

    Lesson-6

---
+++ race.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+const threads = 1000
+
+func main() {
+	var (
+		count int
+		wg    sync.WaitGroup
+	)
+	wg.Add(threads)
+	for i := 0; i < threads; i += 1 {
+		go func() {
+			defer wg.Done()
+			count += 1
+		}()
+	}
+	wg.Wait()
+	fmt.Println(count)
+}
---
+++ trace.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"runtime/trace"
+	"sync"
+)
+
+const threads = 100
+
+func main() {
+	err := trace.Start(os.Stderr)
+
+	if err != nil {
+		log.Fatalf("Error %v", err)
+	}
+	defer trace.Stop()
+
+	var (
+		count int
+		m     = sync.Mutex{}
+		wg    sync.WaitGroup
+	)
+	wg.Add(threads)
+
+	for i := 0; i < threads; i++ {
+		go func() {
+			defer wg.Done()
+			m.Lock()
+			defer m.Unlock()
+			count++
+		}()
+	}
+	wg.Wait()
+
+	fmt.Println("Completed", count)
+}
