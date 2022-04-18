df435ca581407ffd7d8712a5632e94384128145b
ildar valeev <94433322+Valiym@users.noreply.github.com>
2022-04-18 22:12:55 +0300

    Update workers.go

--- workers.go
+++ workers.go
@@ -7,9 +7,9 @@ import (
 
 func main() {
 	total := 0
-	var workers = make(chan struct{}, 1000)
+	var workers = make(chan struct{}, 1)
 
-	for i := 0; i <= 1000; i++ {
+	for i := 1; i <= 1000; i++ {
 		workers <- struct{}{}
 		go func(job int) {
 			defer func() {
